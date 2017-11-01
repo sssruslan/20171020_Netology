1cf9cf9775c2d6c2670aec0a21976e180f948c60
DESKTOP-LJI5H3C\sssruslan <ruslan@bel-ozero.ru>
2017-11-01 20:50:44 +0500

    упрощена десериализация JSON

--- JSON/JSON.py
+++ JSON/JSON.py
@@ -10,28 +10,23 @@
 #                     'items': [{'_id': '545166',
 #                                'description': 'ІЫРбвШ єШЯаР аРббзШвлТРов '
 
-# функция чтения файлов (ГОТОВО)
 def read_files(name):
+    'Функция чтения файлов'
     import json
-    from pprint import pprint
     import chardet
-    import locale
 
     with open(name, 'rb') as f:
         data = f.read()
         result = chardet.detect(data)
-
-    with open(name) as f:
-        data = json.load(f)
-        data_join = ''
-        for i in data['rss']['channel']['items']:
-           data_join += ' ' + i['description']
-        data_join = data_join.encode(locale.getpreferredencoding())
-        original_text = data_join.decode(result['encoding'])
+        data = data.decode(result['encoding'])
+        data = json.loads(data) # внимание! load читает файл, loads читает строку
+        original_text = ''
+        for items in data['rss']['channel']['items']:
+           original_text += ' ' + items['description']
         return original_text
 
-# функция подсчета слов длиннее 6 символов (ГОТОВО)
 def count_word(original_text):
+    'функция подсчета слов длиннее 6 символов'
     to_list = original_text.split(' ')
     word_value = {}
     for word in to_list: 
@@ -42,8 +37,8 @@ def count_word(original_text):
                 word_value[word] = 1
     return word_value # возвращаем словарь {слово:количество}
 
-# функция сортировки и вывода ТОП-10 
 def sort_top(word_value):
+    'функция сортировки и вывода ТОП-10' 
     l = lambda word_value: word_value[1]
     sort_list = sorted(word_value.items(), key = l, reverse = True)
     count = 1
@@ -55,18 +50,18 @@ def sort_top(word_value):
             break
     return top_10
 
-# главная функция: запрашивает имя файла, запускает другие функции (ГОТОВО)
 def main():
-        while True:
-            name = input('Введите имя файла: newsfr.json, newsit.json, newsafr.json, newscy.json. Выход - exit: ')
-            if name == 'newsfr.json' or name == 'newsit.json' or name == 'newsafr.json' or name == 'newscy.json':
-                print('Идет обработка файла ...')
-                top_10 = sort_top(count_word(read_files(name)))
-                for i in top_10.values():
-                    print (i[1], ': ', i[0])
-            elif name == 'exit':
-                break
-            else:
-                print('Некорректный ввод, повторите.')
+    'главная функция: запрашивает имя файла, запускает другие функции'
+    while True:
+        name = input('Введите имя файла: newsfr.json, newsit.json, newsafr.json, newscy.json. Выход - exit: ')
+        if name == 'newsfr.json' or name == 'newsit.json' or name == 'newsafr.json' or name == 'newscy.json':
+            print('Идет обработка файла ...')
+            top_10 = sort_top(count_word(read_files(name)))
+            for i in top_10.values():
+                print (i[1], ': ', i[0])
+        elif name == 'exit':
+            break
+        else:
+            print('Некорректный ввод, повторите.')
         
 main()
